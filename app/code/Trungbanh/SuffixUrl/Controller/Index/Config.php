658d7376b64a5a60e57e5a8203c37bb4e235b09f
trungbanh <trungbanh97@gmail.com>
2023-01-02 16:18:41 +0700

    first commit

---
+++ app/code/Trungbanh/SuffixUrl/Controller/Index/Config.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Trungbanh\SuffixUrl\Controller\Index;
+
+class Config extends \Magento\Framework\App\Action\Action
+{
+
+  protected $helperData;
+
+  public function __construct(
+    \Magento\Framework\App\Action\Context $context,
+    \Trungbanh\SuffixUrl\Helper\Data $helperData
+
+  ) {
+    $this->helperData = $helperData;
+    return parent::__construct($context);
+  }
+
+  public function execute()
+  {
+
+    // TODO: Implement execute() method.
+
+    echo $this->helperData->getGeneralConfig('enable');
+    echo $this->helperData->getGeneralConfig('display_text');
+    exit();
+  }
+}
---
+++ app/code/Trungbanh/SuffixUrl/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+  \Magento\Framework\Component\ComponentRegistrar::MODULE,
+  'Trungbanh_SuffixUrl',
+  __DIR__
+);
